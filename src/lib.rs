fc8d30f5fbc9f3a0debd5c92b02851c9cbe57aad
Roman <roman@opentensor.dev>
2024-09-23 23:32:42 -0700

    add funcs to the pymodule in the lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,5 +16,7 @@ fn bittensor_wallet(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<wallet::Wallet>()?;
     m.add_function(wrap_pyfunction!(utils::get_ss58_format, m)?)?;
     m.add_function(wrap_pyfunction!(utils::is_valid_ss58_address, m)?)?;
+    m.add_function(wrap_pyfunction!(utils::is_valid_ed25519_pubkey, m)?)?;
+    m.add_function(wrap_pyfunction!(utils::is_valid_bittensor_address_or_public_key, m)?)?;
     Ok(())
 }
