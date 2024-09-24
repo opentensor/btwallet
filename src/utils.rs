dbe7f66bba5da50f70422bdb1d3b9c91c352b1b0
Roman <roman@opentensor.dev>
2024-09-23 23:29:52 -0700

    utils done

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyString};
 use std::str;
-
 use sp_core::crypto::{Ss58Codec, AccountId32};
+
+use crate::keypair::Keypair;
+
 // use pyo3::exceptions::PyException;
-// use crate::keypair::Keypair;
-// use pyo3::{pyfunction, pymethods, pymodule, wrap_pyfunction, PyErr, PyResult, Python};
 // use pyo3::prelude::PyModule;
 // use sp_core::crypto::Ss58Codec;
 // use sp_core::crypto::Ss58AddressFormat;
@@ -24,11 +25,17 @@ pub fn get_ss58_format(ss58_address: &str) -> PyResult<u16> {
     }
 }
 
-/// Function to validate a given SS58 address
+/// Checks if the given address is a valid ss58 address.
+///
+/// Args:
+///     address(str): The address to check.
+///
+/// Returns:
+///     True if the address is a valid ss58 address for Bittensor, False otherwise.
 #[pyfunction]
 pub fn is_valid_ss58_address(address: &str) -> PyResult<bool> {
     if address.is_empty() {
-        // hypothetically there could be a debug log, but not a print
+        // Possibly there could be a debug log, but not a print
         // println!("The given address is empty");
         return Ok(false);
     }
@@ -36,39 +43,81 @@ pub fn is_valid_ss58_address(address: &str) -> PyResult<bool> {
     match sp_core::sr25519::Public::from_ss58check(address) {
         Ok(_) => Ok(true),
         Err(_) => {
-            // hypothetically there could be a debug log, but not a print
+            // Possibly there could be a debug log, but not a print
             // println!("Invalid SS58 address format");
             Ok(false)
         }
     }
 }
 
-//
-// fn is_valid_ed25519_pubkey(public_key: &str) -> bool {
-//     let valid_length = public_key.len() == 64 || public_key.len() == 66;
-//     if valid_length { // TODO: when keypair supports
-//         // let keypair = Keypair::public_key(public_key);
-//         // match keypair {
-//         //     Ok(_) => return true,
-//         //     Err(_) => return false,
-//         // }
-//     }
-//     false
-// }
+///    Checks if the given public_key is a valid ed25519 key.
+///
+///     Args:
+///         public_key(Union[str, bytes]): The public_key to check.
+///
+///     Returns:
+///         True if the public_key is a valid ed25519 key, False otherwise.
+#[pyfunction]
+pub fn is_valid_ed25519_pubkey(public_key: &Bound<'_, PyAny>) -> PyResult<bool> {
+    Python::with_gil(|_py| {
+        if public_key.is_instance_of::<PyString>() {
+            let pub_key_string: &str = public_key.extract()?;
+            if pub_key_string.len() != 64 && pub_key_string.len() != 66 {
+                // Possibly need to log the error for debug (a public_key should be 64 or 66 characters)
+                return Ok(false);
+            }
+        } else if public_key.is_instance_of::<PyBytes>() {
+            let pub_key_bytes: &[u8] = public_key.extract()?;
+            if pub_key_bytes.len() != 32 {
+                // Possibly need to log the error for debug (a public_key should be 32 bytes)
+                return Ok(false);
+            }
+        } else {
+            // Possibly need to log the error for debug (public_key must be a string or bytes)
+            return Ok(false);
+        }
 
-// // #[pyfunction]
-// fn is_valid_bittensor_address_or_public_key(address: &[u8]) -> Result<bool, &'static str> {
-//     match str::from_utf8(address) {
-//         Ok(a) => {
-//             if a.starts_with("0x") {
-//                 Ok(is_valid_ed25519_pubkey(a))
-//             } else {
-//                 Ok(is_valid_ss58_address(a))
-//             }
-//         },
-//         Err(_) => Err("Not a valid string"),
-//     }
-// }
+        let pub_key_var = Some(public_key.to_string());
+
+        let keypair_result = Keypair::new(None, pub_key_var, None, 42, None, 1);
+
+        match keypair_result {
+            Ok(keypair) => {
+                if keypair.ss58_address()?.is_some() {
+                    return Ok(true);
+                }
+                Ok(false)
+            }
+            Err(_) => Ok(false),
+        }
+    })
+}
+
+
+///    Checks if the given address is a valid destination address.
+///
+///     Args:
+///         address(Union[str, bytes]): The address to check.
+///
+///     Returns:
+///         True if the address is a valid destination address, False otherwise.
+#[pyfunction]
+pub fn is_valid_bittensor_address_or_public_key(address: &Bound<'_, PyAny>) -> PyResult<bool> {
+    Python::with_gil(|_py| {
+        if address.is_instance_of::<PyString>() {
+            let address_str = &address.to_string();
+            if address_str.starts_with("0x") {
+                is_valid_ed25519_pubkey(address)
+            } else {
+                is_valid_ss58_address(address_str)
+            }
+        } else if address.is_instance_of::<PyBytes>() {
+            is_valid_ed25519_pubkey(address)
+        } else {
+            Ok(false)
+        }
+    })
+}
 
 #[cfg(test)]
 mod tests {
