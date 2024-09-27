90d343c53e1ea49a7995f8d24dee5e83e87ffe08
Roman <roman@opentensor.dev>
2024-09-26 22:04:54 -0700

    add wallet::Wallet.regenerate_coldkeypub, coldkey_file, coldkeypub_file, set_coldkey, set_coldkeypub, get_coldkeypub, get_coldkey, get_hotkey, regenerate_coldkey

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,11 +1,15 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyType};
+use pyo3::types::{PyString, PyType};
+
+use std::ops::Deref;
 
 use colored::Colorize;
+
 use crate::config::Config;
 use crate::constants::{BT_WALLET_HOTKEY, BT_WALLET_NAME, BT_WALLET_PATH};
 use crate::keypair::Keypair;
 use crate::keyfile::Keyfile;
+use crate::utils::{is_valid_bittensor_address_or_public_key};
 
 use dirs::home_dir;
 
@@ -34,7 +38,7 @@ pub struct Wallet {
     pub path: String,
     pub hotkey: String,
     pub config: Option<Config>,
-    // pub hotkey_str: String
+
     _coldkey: Option<Keypair>,
     _coldkeypub: Option<Keypair>,
     _hotkey: Option<Keypair>,
@@ -60,21 +64,24 @@ impl Wallet {
         })
     }
 
-    fn __repr__(&self) -> PyResult<String> {
+    fn __str__(&self) -> PyResult<String> {
         Ok(format!(
-            "Wallet(name='{:}', path='{:}', hotkey='{:}')",
-            self.name, self.path, self.hotkey
+            "Wallet (Name: '{:}', Hotkey: '{:}', Path: '{:}')",
+            self.name, self.hotkey, self.path
         ))
     }
 
-    fn __str__(&self) -> PyResult<String> {
-        self.__repr__()
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "name: '{:}', hotkey: '{:}', path: '{:}'",
+            self.name, self.hotkey, self.path
+        ))
     }
 
     /// Get config from the argument parser.
     #[classmethod]
     pub fn config(cls: &PyType) -> PyResult<Config> {
-        unimplemented!()
+        Config::new(None, None, None)
     }
 
     /// Print help to stdout.
@@ -109,7 +116,7 @@ impl Wallet {
     pub fn hotkey_file(&self) -> PyResult<Keyfile> {
         // get home dir
         let home = home_dir().ok_or_else(|| {
-            pyo3::exceptions::PyRuntimeError::new_err("Failed to get home directory")
+            pyo3::exceptions::PyRuntimeError::new_err("Failed to get user home directory.")
         })?;
 
         // concatenate wallet path
@@ -123,14 +130,36 @@ impl Wallet {
 
     /// Property that returns the coldkey file.
     #[getter]
-    pub fn coldkey_file(&self, py: Python) -> PyResult<Keyfile> {
-        unimplemented!()
+    pub fn coldkey_file(&self) -> PyResult<Keyfile> {
+        // get home dir
+        let home = home_dir().ok_or_else(|| {
+            pyo3::exceptions::PyRuntimeError::new_err("Failed to get user home directory.")
+        })?;
+
+        // concatenate wallet path
+        let wallet_path = home.join(&self.path).join(&self.name);
+
+        // concatenate hotkey path
+        let coldkey_path = wallet_path.join("coldkey");
+
+        Keyfile::new(coldkey_path.to_string_lossy().into_owned(), self.name.clone())
     }
 
     /// Property that returns the coldkeypub file.
     #[getter]
-    pub fn coldkeypub_file(&self, py: Python) -> PyResult<Keyfile> {
-        unimplemented!()
+    pub fn coldkeypub_file(&self) -> PyResult<Keyfile> {
+        // get home dir
+        let home = home_dir().ok_or_else(|| {
+            pyo3::exceptions::PyRuntimeError::new_err("Failed to get user home directory.")
+        })?;
+
+        // concatenate wallet path
+        let wallet_path = home.join(&self.path).join(&self.name);
+
+        // concatenate hotkey path
+        let coldkeypub_path = wallet_path.join("coldkeypub.txt");
+
+        Keyfile::new(coldkeypub_path.to_string_lossy().into_owned(), self.name.clone())
     }
 
     // TODO: the same problem as in keyfile.rs with the same name items withing one struct.
@@ -154,39 +183,45 @@ impl Wallet {
 
     /// Sets the hotkey for the wallet.
     #[pyo3(signature = (keypair, encrypt=true, overwrite=false))]
-    pub fn set_coldkey(&self, keypair: Keypair, encrypt: bool, overwrite: bool, py: Python) -> PyResult<()> {
-        unimplemented!()
+    pub fn set_coldkey(&mut self, keypair: Keypair, encrypt: bool, overwrite: bool, py: Python) -> PyResult<()> {
+        self._coldkey = Some(keypair.clone());
+        self.coldkey_file()?.set_keypair(keypair, encrypt, overwrite, None, py)
     }
 
     /// Sets the coldkeypub for the wallet.
     #[pyo3(signature = (keypair, encrypt=false, overwrite=false))]
-    pub fn set_coldkeypub(&self, keypair: Keypair, encrypt: bool, overwrite: bool, py: Python) -> PyResult<()> {
-        unimplemented!()
+    pub fn set_coldkeypub(&mut self, keypair: Keypair, encrypt: bool, overwrite: bool, py: Python) -> PyResult<()> {
+
+        let _ss58_address = keypair.ss58_address()?.unwrap();
+        let _coldkeypub_keypair = Keypair::new(Some(_ss58_address), None, None, 42, None, 1)?;
+
+        self._coldkeypub = Some(_coldkeypub_keypair.clone());
+        self.coldkeypub_file()?.set_keypair(_coldkeypub_keypair.clone(), encrypt, overwrite, None, py)
     }
 
     /// Sets the hotkey for the wallet.
     #[pyo3(signature = (keypair, encrypt=false, overwrite=false))]
     pub fn set_hotkey(&mut self, keypair: Keypair, encrypt: bool, overwrite: bool, py: Python) -> PyResult<()> {
         self._hotkey = Some(keypair.clone());
-        self.hotkey_file()?.set_keypair(keypair, encrypt, overwrite, None, py)
+        self.hotkey_file()?.set_keypair(keypair.clone(), encrypt, overwrite, None, py)
     }
 
     /// Gets the coldkey from the wallet.
-    #[pyo3(signature = (password))]
-    pub fn get_coldkey(&self, password: Option<String>) -> PyResult<Keypair> {
-        unimplemented!()
+    #[pyo3(signature = (password = None))]
+    pub fn get_coldkey(&self, password: Option<String>, py: Python) -> PyResult<Keypair> {
+        self.coldkey_file()?.get_keypair(password, py)
     }
 
     /// Gets the coldkeypub from the wallet.
-    #[pyo3(signature = (password))]
-    pub fn get_coldkeypub(&self, password: Option<String>) -> PyResult<Keypair> {
-        unimplemented!()
+    #[pyo3(signature = (password = None))]
+    pub fn get_coldkeypub(&self, password: Option<String>, py: Python) -> PyResult<Keypair> {
+        self.coldkeypub_file()?.get_keypair(password, py)
     }
 
     /// Gets the hotkey from the wallet.
-    #[pyo3(signature = (password))]
-    pub fn get_hotkey(&self, password: Option<String>) -> PyResult<Keypair> {
-        unimplemented!()
+    #[pyo3(signature = (password = None))]
+    pub fn get_hotkey(&self, password: Option<String>, py: Python) -> PyResult<Keypair> {
+        self.hotkey_file()?.get_keypair(password, py)
     }
 
     /// Creates coldkey from uri string, optionally encrypts it with the user-provided password.
@@ -229,7 +264,7 @@ impl Wallet {
     /// Unlocks the coldkey.
     pub fn unlock_coldkey(&mut self, py: Python) -> PyResult<Keypair> {
         if self._coldkey.is_none() {
-            let coldkey_file = self.coldkey_file(py)?;
+            let coldkey_file = self.coldkey_file()?;
             self._coldkey = Some(coldkey_file.get_keypair(None, py)?);
         }
         let _coldkey = self._coldkey.clone().ok_or(pyo3::exceptions::PyOSError::new_err("Coldkey file doesn't exist."))?;
@@ -239,7 +274,7 @@ impl Wallet {
     /// Unlocks the coldkeypub.
     pub fn unlock_coldkeypub(&mut self, py: Python) -> PyResult<Keypair> {
         if self._coldkeypub.is_none() {
-            let coldkeypub_file = self.coldkeypub_file(py)?;
+            let coldkeypub_file = self.coldkeypub_file()?;
             self._coldkeypub = Some(coldkeypub_file.get_keypair(None, py)?);
         }
         let _coldkeypub = self._coldkeypub.clone().ok_or(pyo3::exceptions::PyOSError::new_err("Coldkey file doesn't exist."))?;
@@ -300,19 +335,59 @@ impl Wallet {
 
     /// Regenerates the coldkeypub from the passed ``ss58_address`` or public_key and saves the file. Requires either ``ss58_address`` or public_key to be passed.
     #[pyo3(signature = (ss58_address = None, public_key = None, overwrite = false))]
-    pub fn regenerate_coldkeypub(&self, ss58_address: Option<String>, public_key: Option<PyObject>, overwrite: bool, py: Python) -> PyResult<Wallet> {
-        unimplemented!()
+    pub fn regenerate_coldkeypub(&mut self, ss58_address: Option<String>, public_key: Option<String>, overwrite: bool, py: Python) -> PyResult<Self> {
+
+        if ss58_address.is_none() && public_key.is_none() {
+            return Err(pyo3::exceptions::PyValueError::new_err("Either ss58_address or public_key must be passed."));
+        }
+
+        // convert from Option<String> to &Bound<PyAny> for `is_valid_bittensor_address_or_public_key`
+        let address_to_string = ss58_address.as_ref().or(public_key.as_ref());
+        let binding_py_string = PyString::new_bound(py, address_to_string.unwrap().as_str());
+        let address_to_check: &Bound<PyAny> = binding_py_string.as_ref();
+
+        if !is_valid_bittensor_address_or_public_key(address_to_check)? {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                format!(
+                    "Invalid {}.",
+                    if ss58_address.is_some() { "ss58_address" } else { "public_key" }
+                )
+            ));
+        }
+
+        let keypair = Keypair::new(ss58_address, public_key, None, 42, None, 1)?;
+
+        self.set_coldkeypub(keypair, overwrite, false, py)?;
+        Ok(self.clone())
     }
 
     /// Regenerates the coldkey from the passed mnemonic or seed, or JSON encrypts it with the user's password and saves the file.
-    #[pyo3(signature = (use_password = true, overwrite = false, suppress = false))]
-    pub fn regenerate_coldkey(&self, use_password: bool, overwrite: bool, suppress: bool, py: Python) -> PyResult<Wallet> {
-        unimplemented!()
+    #[pyo3(signature = (mnemonic=None, seed=None, json=None, use_password=true, overwrite=false, suppress=false))]
+    pub fn regenerate_coldkey(&mut self, mnemonic: Option<String>, seed: Option<String>, json: Option<(String, String)>, use_password: bool, overwrite: bool, suppress: bool, py: Python) -> PyResult<Self> {
+        let keypair = if let Some(mnemonic) = mnemonic {
+            // mnemonic
+            let keypair = Keypair::create_from_mnemonic(&mnemonic)?;
+            if !suppress {
+                display_mnemonic_msg(mnemonic, "coldkey");
+            }
+            keypair
+        } else if let Some(seed) = seed {
+            // seed
+            Keypair::create_from_seed(&seed)?
+        } else if let Some((json_data, passphrase)) = json {
+            // json_data + passphrase
+            Keypair::create_from_encrypted_json(&json_data, &passphrase)?
+        } else {
+            return Err(pyo3::exceptions::PyValueError::new_err("Must pass either mnemonic, seed, or json."));
+        };
+
+        self.set_coldkey(keypair.clone(), use_password, overwrite, py)?;
+        self.set_coldkeypub(keypair.clone(), false, overwrite, py)?;
+        Ok(self.clone())
     }
 
     /// Regenerates the hotkey from passed mnemonic or seed, encrypts it with the user's password and saves the file.
-    #[pyo3(signature = (mnemonic=None, seed=None, json=None, use_password=true, overwrite=false, suppress=false)
-    )]
+    #[pyo3(signature = (mnemonic=None, seed=None, json=None, use_password=true, overwrite=false, suppress=false))]
     pub fn regenerate_hotkey(&mut self, mnemonic: Option<String>, seed: Option<String>, json: Option<(String, String)>, use_password: bool, overwrite: bool, suppress: bool, py: Python) -> PyResult<Self> {
         let keypair = if let Some(mnemonic) = mnemonic {
             // mnemonic
@@ -328,7 +403,7 @@ impl Wallet {
             // json_data + passphrase
             Keypair::create_from_encrypted_json(&json_data, &passphrase)?
         } else {
-            return Err(pyo3::exceptions::PyValueError::new_err("Must pass either mnemonic, seed, or json"));
+            return Err(pyo3::exceptions::PyValueError::new_err("Must pass either mnemonic, seed, or json."));
         };
 
         self.set_hotkey(keypair, use_password, overwrite, py)?;
