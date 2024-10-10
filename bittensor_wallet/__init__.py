64a13977ed6bf03e67ff5fd2e198bec8411132f0
Roman <roman@opentensor.dev>
2024-10-09 17:10:28 -0700

    fix imports

--- bittensor_wallet/__init__.py
+++ bittensor_wallet/__init__.py
@@ -1,14 +1,16 @@
 from bittensor_wallet.bittensor_wallet import (
-    Config as Config,
-    Keyfile as Keyfile,
-    Keypair as Keypair,
-    Wallet as Wallet,
-    utils as utils,
-    config as config,
-    errors as errors,
-    keypair as keypair,
-    keyfile as keyfile,
-    wallet as wallet,
+    # classes
+    Config,
+    Keyfile,
+    Keypair,
+    Wallet,
+    # modules
+    config,
+    errors,
+    keyfile,
+    keypair,
+    utils,
+    wallet,
 )
 
 __version__ = "2.0.0"
