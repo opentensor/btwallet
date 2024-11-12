045d0e6e30d37b49ff6fc9b15522ccd4248f3253
Cameron Fairchild <cameron@opentensor.ai>
2024-11-12 16:34:13 -0500

    Fix ruff (#62)

--- bittensor_wallet/__init__.py
+++ bittensor_wallet/__init__.py
@@ -1,16 +1,16 @@
 from bittensor_wallet.bittensor_wallet import (
     # classes
-    Config,
-    Keyfile,
-    Keypair,
-    Wallet,
+    Config as Config,
+    Keyfile as Keyfile,
+    Keypair as Keypair,
+    Wallet as Wallet,
     # modules
-    config,
-    errors,
-    keyfile,
-    keypair,
-    utils,
-    wallet,
+    config as config,
+    errors as errors,
+    keyfile as keyfile,
+    keypair as keypair,
+    utils as utils,
+    wallet as wallet,
 )
 
 __version__ = "2.0.0"
