490e3785892622cd6db62e45fc3ce6926fa26e34
Cameron Fairchild <cameron@opentensor.ai>
2025-01-24 15:40:18 -0500

    chore: ruff

--- bittensor_wallet/__init__.pyi
+++ bittensor_wallet/__init__.pyi
@@ -1,32 +1,40 @@
 from typing import Optional, Final
 
 class Keypair:
-    #[getter]
+    # [getter]
     ss58_address: Final[Optional[str]]
-    
-    #[getter]
+
+    # [getter]
     public_key: Final[Optional[bytes]]
-    
-    #[getter]
+
+    # [getter]
     ss58_format: Final[int]
 
-    #[getter]
+    # [getter]
     seed_hex: Final[Optional[bytes]]
 
-    #[getter] #[setter]
+    # [getter] #[setter]
     crypto_type: int
 
-    #[getter]
+    # [getter]
     mnemonic: Final[Optional[str]]
 
-    #[getter]
+    # [getter]
     private_key: Final[Optional[bytes]]
 
-    def __init__(self, ss58_address: Optional[str] = None, public_key: Optional[str] = None, private_key: Optional[str] = None, ss58_format: int = 42, seed_hex: Optional[bytes] = None, crypto_type: int = 1) -> None:
+    def __init__(
+        self,
+        ss58_address: Optional[str] = None,
+        public_key: Optional[str] = None,
+        private_key: Optional[str] = None,
+        ss58_format: int = 42,
+        seed_hex: Optional[bytes] = None,
+        crypto_type: int = 1,
+    ) -> None:
         pass
 
-    #[new]
-    #[pyo3(signature = (ss58_address=None, public_key=None, private_key=None, ss58_format=42, seed_hex=None, crypto_type=1))]
+    # [new]
+    # [pyo3(signature = (ss58_address=None, public_key=None, private_key=None, ss58_format=42, seed_hex=None, crypto_type=1))]
     def __new__(
         ss58_address: Optional[str] = None,
         public_key: Optional[str] = None,
@@ -34,48 +42,48 @@ class Keypair:
         ss58_format: int = 42,
         seed_hex: Optional[bytes] = None,
         crypto_type: int = 1,
-    ) -> 'Keypair':
+    ) -> "Keypair":
         pass
-        
 
-    #[staticmethod]
-    #[pyo3(signature = (n_words=12))]
+    # [staticmethod]
+    # [pyo3(signature = (n_words=12))]
     @staticmethod
     def generate_mnemonic(n_words: int = 12) -> str:
         pass
 
-    #[staticmethod]
+    # [staticmethod]
     @staticmethod
-    def create_from_mnemonic(mnemonic: str) -> 'Keypair':
+    def create_from_mnemonic(mnemonic: str) -> "Keypair":
         pass
 
-
-    #[staticmethod]
+    # [staticmethod]
     @staticmethod
-    def create_from_seed(seed: bytes) -> 'Keypair':
+    def create_from_seed(seed: bytes) -> "Keypair":
         pass
 
-
-    #[staticmethod]
+    # [staticmethod]
     @staticmethod
-    def create_from_private_key(private_key: str) -> 'Keypair':
+    def create_from_private_key(private_key: str) -> "Keypair":
         pass
 
-
-    #[staticmethod]
+    # [staticmethod]
     @staticmethod
-    def create_from_encrypted_json(json_data: str, passphrase: str) -> 'Keypair':
+    def create_from_encrypted_json(json_data: str, passphrase: str) -> "Keypair":
         pass
 
-    #[staticmethod]
+    # [staticmethod]
     @staticmethod
-    def create_from_uri(uri: str) -> 'Keypair':
+    def create_from_uri(uri: str) -> "Keypair":
         pass
 
-    #[pyo3(signature = (data))]
-    def sign(self, data: str | bytes | list[int] | 'scalec_codec.ScaleBytes') -> bytes:
+    # [pyo3(signature = (data))]
+    def sign(self, data: str | bytes | list[int] | "scalec_codec.ScaleBytes") -> bytes:
         pass
 
-    #[pyo3(signature = (data, signature))]
-    def verify(self, data: str | bytes | list[int] | 'scalec_codec.ScaleBytes', signature: str | bytes | list[int]) -> bool:
+    # [pyo3(signature = (data, signature))]
+    def verify(
+        self,
+        data: str | bytes | list[int] | "scalec_codec.ScaleBytes",
+        signature: str | bytes | list[int],
+    ) -> bool:
         pass
